241e8d2841fd895f73fc33c9b57ac16b78f5624f
Lucaxcm <lucasveigacm@gmail.com>
2025-03-07 15:37:06 -0300

    Atualização do Código

--- Barra De Carregamento.c
+++ Barra De Carregamento.c
@@ -2,19 +2,32 @@
 #include <unistd.h>
 
 int main() {
-    int i = 0, j = 0, k;
+    int i = 0, j = 0, k = 0;
     float por = 0;
 
     while (j != 1) {
-        while (i != 56) {
-            for (k = 0; k < i; k++) {
-                printf("\U0001F40D");}
-            for (int k = i; k < 10; k++) {
-                printf("\r"); 
-            }
+        while (i != 11) {
+            for (k = 0; k < (i+1); k++){
+                printf("\U0001F40D ");}
+                
+            for (int k = i; k < 10; k++){
+                printf("\r"); }
+            
             por = i * 100.0 / 10;
+            
+            printf("\r");
             printf("[ %.0f%% ]", por);
+            
             printf(" ");
+            
             fflush(stdout);
+            
             usleep(1500000); 
-            i++;}j++;}}
+        
+        i++;
+            
+        }
+            
+        j++;
+        
+    }
